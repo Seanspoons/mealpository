35065655ca801930f3be76f2aeccf721f3fe6891
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-01-25 16:50:12 -0800

    Add top of homepage
    - Added the main section of main page

--- mealpository-frontend/tailwind.config.js
+++ mealpository-frontend/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+      'ebe9e5': '#ebe9e5',
+      '013a63': '#013a63',
+    },
+      fontSize: {
+        'mb-button': '1.04rem',
+      },
+      maxWidth: {
+        'mb-home-text': '216px',
+        'sm-home-text': '400px',
+        'md-home-text': '216px',
+        'lg-home-text': '216px',
+      },
+      spacing: {
+        '22': '5.5rem',
+      }
+  },
   },
   plugins: [],
 }
