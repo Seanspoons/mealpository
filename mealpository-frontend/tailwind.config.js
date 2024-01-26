020695a53c31b08609f7843a9ab063c5eb29ee67
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-01-25 21:43:18 -0800

    Added "how it works" section and "get started" section
    - Both sections added with functionality up to lg
    - xl and more to come soon

--- mealpository-frontend/tailwind.config.js
+++ mealpository-frontend/tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
       backgroundColor: {
       'ebe9e5': '#ebe9e5',
       '013a63': '#013a63',
+      '597385': '#597385',
     },
       fontSize: {
         'mb-button': '1.04rem',
       },
       maxWidth: {
         'mb-home-text': '216px',
+        'mb-works-text': '266px',
+        'sm-works-text': '325px',
         'sm-home-text': '400px',
+        'md-works-text': '550px',
         'md-home-text': '700px',
         'lg-home-text': '800px',
         'xl-home-text': '800px',
@@ -21,6 +25,9 @@ module.exports = {
         '21': '5.25rem',
         '22': '5.5rem',
         '23': '5.75rem',
+      },
+      screens: {
+        'lg-xl': '1075px'
       }
   },
   },
