87846495602eb67cd6a74710c4cfd03433e1c47a
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-02-05 22:05:37 -0800

    Created signup and login APIs

--- mealpository_backend/authentication/urls.py
+++ mealpository_backend/authentication/urls.py
@@ -1,5 +1,8 @@
-from django.urls import path
+from django.urls import re_path
+from . import views
 
 urlpatterns = [
-    
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test_token', views.test_token),
 ]
--- mealpository_backend/mealpository_backend/settings.py
+++ mealpository_backend/mealpository_backend/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
-    'corsheaders',
+    'rest_framework.authtoken',
     'authentication',
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
