07a6c104ae1a51dcdbe49ff6d9fbe3184ef85fc9
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-02-09 22:38:56 -0800

    Created authentication token testing api

--- mealpository_backend/authentication/views.py
+++ mealpository_backend/authentication/views.py
@@ -1,11 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 def login(request):
@@ -29,5 +31,7 @@ def signup(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def test_token(request):
-    return Response({})
+    return Response({"passed for {}".format(request.user.email)})
