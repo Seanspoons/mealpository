f20fc7e77194bfa11f0647d59182097b30897a43
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-02-11 14:37:41 -0800

    Added user feedback for login errors

--- mealpository-frontend/src/app/authentication/login/login.component.ts
+++ mealpository-frontend/src/app/authentication/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,10 +12,15 @@ import { AuthenticationService } from '../services/authentication.service';
 export class LoginComponent {
 
   loginForm: FormGroup;
+  loginError: boolean;
+  parsingError: boolean;
 
   constructor(
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private router: Router
   ) {
+    this.loginError = false;
+    this.parsingError = false;
     let loginFormControls = { /* Add more validators */
       email: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
@@ -28,6 +34,9 @@ export class LoginComponent {
       const email = this.loginForm.get('email')!.value;
       const password = this.loginForm.get('password')!.value;
 
+      const loginErrorDiv = document.getElementById('login-error');
+      const parsingErrorDiv = document.getElementById('parsing-error');
+
       this.authenticationService.login(email, password).subscribe(
         response => {
           var authToken = response.token;
@@ -35,12 +44,37 @@ export class LoginComponent {
           this.authenticationService.verifyToken(authToken).subscribe(
             response => {
               console.log("API Response: ", response);
+              if(this.loginError === true) {
+                loginErrorDiv!.style.display = 'none';
+                this.loginError = false;
+              }
+              if(this.parsingError === true) {
+                parsingErrorDiv!.style.display = 'none';
+                this.parsingError = false;
+              }
+              this.router.navigateByUrl('home-loggedin');
+            },
+            error => {
+              console.error('Token authentication error:', error);
+              if(this.loginError === true) {
+                loginErrorDiv!.style.display = 'none';
+                this.loginError = false;
+              }
+              parsingErrorDiv!.style.display = 'block';
+              this.parsingError = true;
             }
           );
         },
         error => {
           console.error('Login error:', error);
-          // Handle error if necessary
+          if(this.parsingError === true) {
+            parsingErrorDiv!.style.display = 'none';
+            this.parsingError = false;
+          }
+          if(this.loginError === false) {
+            loginErrorDiv!.style.display = 'block';
+            this.loginError = true;
+          }
         }
       );
     }
