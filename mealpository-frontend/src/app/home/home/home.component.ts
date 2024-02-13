54523999383918e286aee02ce5ba03db5f641dbd
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-02-13 15:14:26 -0800

    Added functionality to home page scroll button

--- mealpository-frontend/src/app/home/home/home.component.ts
+++ mealpository-frontend/src/app/home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../authentication/services/authentication.service';
 
@@ -10,17 +10,27 @@ import { AuthenticationService } from '../../authentication/services/authenticat
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('howItWorks') howItWorks!: ElementRef;
   loggedIn: boolean;
 
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
     ) {
       this.loggedIn = this.authenticationService.isLoggedIn();
     }
 
   ngOnInit(): void {
     this.loggedIn = this.authenticationService.isLoggedIn();
+  }
+
+  onScrollClick(): void {
+    console.log("Scroll click");
+    const yOffset = -60;
+    const targetElement = this.howItWorks.nativeElement;
+    const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({ top: y, behavior: 'smooth' });
   }
 
   onLoginClick(): void {
