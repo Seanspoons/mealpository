301511cb1b0900b04af31f8f2dcbf2a8645493c6
Sean Wotherspoon <98927896+Seanspoons@users.noreply.github.com>
2024-02-14 17:09:45 -0800

    Created Recipes Module and Component

--- mealpository-frontend/src/app/app.routes.ts
+++ mealpository-frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeLoggedinComponent } from './home/home-loggedin/home-loggedin.compon
 import { LoginComponent } from './authentication/login/login.component';
 import { SignupComponent } from './authentication/signup/signup.component';
 import { AuthGuard } from './authentication/auth.guard';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeLoggedinComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard] },
     
 ];
 
---
+++ mealpository-frontend/src/app/recipes/recipes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipesComponent } from './recipes/recipes.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RecipesComponent,
+  ]
+})
+export class RecipesModule { }
